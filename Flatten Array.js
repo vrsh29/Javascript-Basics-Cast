51f5d6d61fa08808e9d3cd80370d85cfd87fcac6
varsha.m.nagaraj <varsha.m.nagaraj@accenture.com>
2018-04-24 17:16:39 +0530

    flatten

---
+++ Flatten Array.js
@@ -0,0 +1,18 @@
+// CHALLENGE 3: FLATTEN ARRAY
+// Take an array of arrays and flatten to a single array
+// ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
+
+function flattenArray(arrays) {
+
+// Solution 1:
+//     return arrays.reduce((a, b) => a.concat(b));
+//     console.log(flattenArray);
+// }
+
+// Solution 2 : 
+
+
+// Call Function
+const output = flattenArray([[1, 2], [3, 4], [5, 6], [7]]);
+
+console.log(output);
